6bf59fa678a38b08a4185f09f879ef034d54218e
Franck Olivier <franck.aboa@smile.ci>
2018-03-10 09:25:54 +0000

    On branch master 	new file:   welcome-noobs-deploy.sh

---
+++ welcome-noobs-deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Variables according to you Distribution
+# [RedHat] var=yum
+# [Debian] var=yum
+var=yum
+
+$var -y install deltarpm -y
+$var  groups install "Development Tools" -y
+$var -y install release* -y
+$var -y update -y
+$var -y upgrade -y
+
+$var -y install ansible
+
+echo "inventory = /etc/ansible/hosts" >> /etc/ansible/ansible.cfg
+echo "localhost   ansible_connection=local" >> /etc/ansible/hosts
+
+echo "execution du role de configuration des logiciels du noobs"
+sleep 2
+
+#ansible-playbook
+
+ansible-playbook ./welcome-noobs/tests/test.yml -K
